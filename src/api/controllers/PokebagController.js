43e7bc2cc2d4d37cf23636269703b07362f99625
Yobel <vega.yobel.2@gmail.com>
2022-06-08 20:55:59 +0700

    Tes sonar cloud

--- src/api/controllers/PokebagController.js
+++ src/api/controllers/PokebagController.js
@@ -2,6 +2,7 @@ import { getPokemonData } from '../utils';
 
 async function PokebagController(pokemon) {
   const pokemons = await pokemon.map((item) => getPokemonData(item));
+
   return Promise.all(pokemons);
 }
 export default PokebagController;
